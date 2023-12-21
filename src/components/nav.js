b10cb6801875827c22124f3affece6118ee82856
knyhago <knyhago@outlook.com>
2023-12-21 23:37:13 +0000

    2 coomit

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,10 +1,13 @@
 import Sidebar from "./sideList"
+import { Link } from 'react-router-dom';
 
 
 const Nav=()=>{
     return(
         <div className="navbar">
             <a href="#">Blogs App</a>
+            <button> <Link to="/home">Home</Link></button>
+            <button> <Link to="/addBlog">Add Blog</Link></button>
             
 
         </div>
--- src/components/sideList.js
+++ src/components/sideList.js
@@ -1,31 +1,20 @@
-import react from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './sideList.css'
-const Sidebar=()=>{
-    return(
+import './sideList.css';
+
+const Sidebar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
         <div>
-            <div>
-            <button class="openbtn" onClick={open}>&#9776; Open Sidebar</button>
-            </div>
-            <div className='sidebar' id='sidelist'>
-                <a class="close" onClick={close}>&times;</a>
-                <Link to="/home">Home</Link>
-                <Link to="/addBlog">Add Blog</Link>
-            </div>
+            
+            
         </div>
-    )
-}
+    );
+};
 
-const open=()=>{
-    console.log('opened')
-    document.getElementById('sidelist').style.width="250px"
-    document.getElementById('roe').style.marginLeft="250px"
-}
-
-const close=()=>{
-    console.log("closed")
-    document.getElementById('sidelist').style.width="0px"
-    document.getElementById('roe').style.marginLeft="0px"
-}
-
-export default Sidebar
+export default Sidebar;
